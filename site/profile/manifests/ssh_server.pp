eed6ad0304bbe08b5d8610b40471b2d09ba550f1
mnh2005124 <30888012+mnh2005124@users.noreply.github.com>
2022-09-26 16:07:25 +0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDclSgcqu5aC3C/2y3mnn8vLT7JSmbphOT2zR+yAeVyS+EPZIVfdh5WJu8XvFKw95/Rb65XO3ku6WPItg+uL5cbQ1pVto/iC8ul5r4Olt55MNGpDopvFxfr6G02UBSvHxsWbuaajppBxVKkR5Tu/GB5ATBbXSTD1trWCVyt9Hg7/vy7ukSB0pDfAsvdltPVC7vV7P/oPf+mg/i+avmgz0zc4eWuScDLTIip1gpIv5j3DyLZevp5yAqE72Xxj0gMpmK4Ab2c2eMEdS/b+TwS3EDJJvsNWCpMZ4OtzYUi0Pj8M4924sYY1cQcDmxdseEum30s7DEgx2OcyQIVFUeIRdV1',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDclSgcqu5aC3C/2y3mnn8vLT7JSmbphOT2zR+yAeVyS+EPZIVfdh5WJu8XvFKw95/Rb65XO3ku6WPItg+uL5cbQ1pVto/iC8ul5r4Olt55MNGpDopvFxfr6G02UBSvHxsWbuaajppBxVKkR5Tu/GB5ATBbXSTD1trWCVyt9Hg7/vy7ukSB0pDfAsvdltPVC7vV7P/oPf+mg/i+avmgz0zc4eWuScDLTIip1gpIv5j3DyLZevp5yAqE72Xxj0gMpmK4Ab2c2eMEdS/b+TwS3EDJJvsNWCpMZ4OtzYUi0Pj8M4924sYY1cQcDmxdseEum30s7DEgx2OcyQIVFUeIRdV1',
     }
 }
